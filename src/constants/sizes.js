4fc00c1023480937908a0516a6111908e7a0eccf
Rafael S Magalhaes <rafaelsmag@gmail.com>
2018-05-21 22:20:41 -0300

    Adicionado imagem do poster

--- src/constants/sizes.js
+++ src/constants/sizes.js
@@ -1,3 +1,14 @@
 import {Dimensions} from 'react-native';
 
 export const screen = Dimensions.get('screen');
+
+export const SPACE_KILO= 5;
+export const SPACE_MEGA = 10;
+export const SPACE_GIGA = 20;
+export const SPACE_TERA = 30;
+
+
+export const POSTER_CONTAINER_WIDTH = screen.width / 16 * 12;
+export const POSTER_CONTAINER_HEIGHT = screen.height / 5 * 2;
+
+export const POSTER_IMAGE_HEIGHT = POSTER_CONTAINER_HEIGHT;
--- src/modules/home/components/Home.js
+++ src/modules/home/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { SafeAreaView, Text, StyleSheet, View, Image, Dimensions } from 'react-native'
-import { screen } from '../../../constants/sizes';
+import { screen, SPACE_MEGA, SPACE_GIGA, POSTER_IMAGE_HEIGHT, SPACE_TERA, POSTER_CONTAINER_WIDTH, POSTER_CONTAINER_HEIGHT } from '../../../constants/sizes';
 
 class Home extends Component {
 
@@ -21,8 +21,10 @@ class Home extends Component {
     const { item } = this.state;
     return(
       <SafeAreaView style={{ flex: 1 }}>
-        <Image blurRadius={3} source={{ uri: item.image }} style={StyleSheet.flatten([ StyleSheet.absoluteFill, styles.imageBackground])} />
-        <Text>{this.state.item.title}</Text>
+        <Image blurRadius={20} source={{ uri: item.image }} style={StyleSheet.flatten([ StyleSheet.absoluteFill, styles.imageBackground])} />
+        <View style={styles.posterImageWrapper}>
+          <Image source={{ uri: item.image }} style={styles.posterImage}/>
+        </View>
       </SafeAreaView>
     )
   }
@@ -31,6 +33,21 @@ class Home extends Component {
 const styles = StyleSheet.create({
   imageBackground: {
     width: screen.width,
+  },
+  posterImageWrapper: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  posterImage: {
+    borderRadius: 10,
+    height: POSTER_IMAGE_HEIGHT,
+    width: POSTER_IMAGE_HEIGHT*0.674,
+    marginTop: SPACE_TERA,
+    marginHorizontal: SPACE_TERA,
+    resizeMode: Image.resizeMode.contain,
+    borderRadius: 10,
+    elevation: 2,    
   }
 });
 
